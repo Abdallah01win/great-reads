850c4cde68792f5332568e793bb7c793365788d2
Abdallah Bari <abdellah01win@gmail.com>
2022-01-15 18:10:42 +0100

    creat dark and light theme toggel

--- src/index.js
+++ src/index.js
@@ -2,3 +2,34 @@ import { cons } from "./app/firebase";
 //import { button } from "./app/getbooks" 
 console.log('from bindel.js');
 console.log('from compiled with live reloding devserver');
+
+const themeSwitch = document.getElementById('darkThemeToggle');
+const root = document.querySelector(':root');
+let darkTheme = localStorage.getItem("darkTheme")
+const enableDarkTheme= ()=>{
+    document.body.classList.add("darkTheme");
+    localStorage.setItem('darkTheme', 'enabled');
+} 
+const disableDarkTheme= ()=>{
+    document.body.classList.remove("darkTheme");
+    localStorage.setItem('darkTheme', null);
+} 
+
+if(darkTheme === "enabled"){
+    enableDarkTheme();
+    themeSwitch.innerHTML = "<ion-icon name='sunny-outline'></ion-icon>"
+}else{
+    themeSwitch.innerHTML = "<ion-icon name='moon-outline'></ion-icon>"
+}
+
+themeSwitch.addEventListener('click', ()=>{
+    //console.log('test')
+    darkTheme = localStorage.getItem("darkTheme");
+    if(darkTheme !=='enabled'){
+        enableDarkTheme();
+        themeSwitch.innerHTML = "<ion-icon name='sunny-outline'></ion-icon>"
+    } else if (darkTheme !== null){
+        disableDarkTheme();
+        themeSwitch.innerHTML = "<ion-icon name='moon-outline'></ion-icon>"
+    }
+})
